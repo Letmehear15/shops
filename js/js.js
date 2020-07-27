5e1f12605d96a9d2d8a9bed0f13d16b00b739751
Alexandr <alex@MacBook-Pro-Alexandr.local>
2020-07-27 12:28:42 +0200

    chujeta

--- js/js.js
+++ js/js.js
@@ -3,43 +3,14 @@ window.addEventListener('DOMContentLoaded', function() {
     const title = document.querySelector('.main__title');
     const burger = document.querySelector('.burger_menu');
     const menu = document.querySelector('.menu');
-    const search = document.querySelector('.main__search');
-    const input = document.querySelector('.main__search input');
-    const searchImg = document.querySelector('.main__search img');
+    const cards = document.querySelectorAll('.main__item')
+    const cardsTitle = document.querySelectorAll('.item_title')
+    const cardsImg = document.querySelectorAll('.rightSide img')
+    const modal = document.querySelector('.modal__wrapper')
+    const modalTitle = document.querySelector('.modal__title')
+    const modalImg = document.querySelector('.modal__img img')
+    const close = document.querySelector('.close')
 
-    window.addEventListener('scroll', () => {
-        searchImg.style.display = `block`;
-        input.style.paddingLeft = `${44}px`;
-        input.style.fontSize = `${18}px`;
-        if(window.innerWidth >= 1100) return;
-        if(window.pageYOffset > 30) {
-            if(window.innerWidth < 370){
-                search.style.width = `${219}px`;
-                search.style.marginLeft = `auto`;
-                input.style.paddingLeft = `${15}px`;
-                input.style.fontSize = `${14}px`;
-                searchImg.style.display = `none`;
-                return
-            }
-            if(window.innerWidth < 725 && window.innerWidth > 481){
-                search.style.width = `${350}px`;
-                search.style.marginLeft = `auto`;
-                return
-            }
-            if(window.innerWidth < 480){
-                search.style.width = `${250}px`;
-                search.style.marginLeft = `auto`;
-                input.style.fontSize = `${15}px`;
-                input.style.paddingLeft = `${15}px`;
-                searchImg.style.display = `none`;
-                return
-            }
-            search.style.width = `${561}px`;
-            search.style.marginLeft = `auto`;
-            
-        } else search.style.width = `${100}%`
-        
-    })
     burger.addEventListener('click', () => {
         burger.classList.toggle('active_burger');
         if(window.innerWidth < 380) {
@@ -47,6 +18,16 @@ window.addEventListener('DOMContentLoaded', function() {
             return
         }
         menu.classList.toggle('active_menu_tablet');
+    })
+    cards.forEach((el, index) => {
+        el.addEventListener("click", function()  {
+            modal.style.display = 'flex';
+            modalTitle.textContent = cardsTitle[index].textContent;
+            modalImg.src = cardsImg[index].src
+        })
+    })
+    close.addEventListener('click', () => {
+        modal.style.display = 'none'
     })
     menuItems.forEach(el => {
         el.addEventListener('click', (e) => {
